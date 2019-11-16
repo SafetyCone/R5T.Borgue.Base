8c4cdc2301748ed019f323794c16316edb6371ea
MinexAutomation <minex.automation@gmail.com>
2019-11-16 12:43:44 -0800

    Added geography repository and extensions.

---
+++ source/R5T.Borgue.Base/Code/Services/Extensions/IGeographyRepositoryExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using R5T.Corcyra;
+
+
+namespace R5T.Borgue
+{
+    public static class IGeographyRepositoryExtensions
+    {
+        public static GeographyIdentity Add(this IGeographyRepository repository, IEnumerable<LngLat> vertices)
+        {
+            var geography = new Geography()
+            {
+                Identity = GeographyIdentity.New(),
+            };
+
+            geography.Vertices.AddRange(vertices);
+
+            repository.Add(geography);
+
+            return geography.Identity;
+        }
+    }
+}
---
+++ source/R5T.Borgue.Base/Code/Services/Interfaces/IGeographyRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using R5T.Corcyra;
+
+
+namespace R5T.Borgue
+{
+    public interface IGeographyRepository
+    {
+        void Add(Geography geography);
+
+        bool Exists(GeographyIdentity identity);
+
+        Geography Get(GeographyIdentity identity);
+
+        void Delete(GeographyIdentity identity);
+    }
+}

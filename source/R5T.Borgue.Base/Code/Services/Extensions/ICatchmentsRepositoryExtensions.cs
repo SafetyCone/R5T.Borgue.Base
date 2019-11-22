905b888e1b112ee5e3ce665e71fc7909c0e909d4
MinexAutomation <minex.automation@gmail.com>
2019-11-22 13:35:19 -0800

    Renamed "Geography" to "Catchment".

---
+++ source/R5T.Borgue.Base/Code/Services/Extensions/ICatchmentsRepositoryExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using R5T.Corcyra;
+
+
+namespace R5T.Borgue
+{
+    public static class ICatchmentsRepositoryExtensions
+    {
+        public static CatchmentIdentity Add(this ICatchmentsRepository repository, IEnumerable<LngLat> vertices)
+        {
+            var geography = new Catchment()
+            {
+                Identity = CatchmentIdentity.New(),
+            };
+
+            geography.Boundary.AddRange(vertices);
+
+            repository.Add(geography);
+
+            return geography.Identity;
+        }
+    }
+}
---
+++ source/R5T.Borgue.Base/Code/Services/Interfaces/ICatchmentsRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using R5T.Corcyra;
+
+
+namespace R5T.Borgue
+{
+    public interface ICatchmentsRepository
+    {
+        void Add(Catchment geography);
+
+        bool Exists(CatchmentIdentity identity);
+
+        Catchment Get(CatchmentIdentity identity);
+        IEnumerable<Catchment> GetAll();
+        IEnumerable<Catchment> GetAllContainingPoint(LngLat lngLat);
+
+        void Delete(CatchmentIdentity identity);
+    }
+}

1871dfa31e772802424421722103a543eafc3123
Zeph Arvanitis <tasha.arvanitis@gmail.com>
2020-12-23 12:21:24 -0600

    Add IGridUnitsRepository

---
+++ source/R5T.Borgue.Base/Code/Services/Interfaces/IGridUnitsRepository.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using R5T.Corcyra;
+
+
+namespace R5T.Borgue
+{
+    public interface IGridUnitsRepository
+    {
+        Task SetGridUnitsForCatchment(CatchmentIdentity catchmentIdentity);
+
+        Task Add(GridUnit geography);
+        Task Delete(GridUnitIdentity identity);
+        Task<bool> Exists(GridUnitIdentity identity);
+        Task<GridUnit> Get(GridUnitIdentity identity);
+        Task<IEnumerable<GridUnit>> GetAll();
+
+        // Getters and setters for catchment components
+        Task SetName(GridUnitIdentity identity, string name);
+        Task<string> GetName(GridUnitIdentity identity);
+        Task SetBoundary(GridUnitIdentity identity, IEnumerable<LngLat> boundaryVertices);
+        Task<IEnumerable<LngLat>> GetBoundary(GridUnitIdentity identity);
+    }
+}
